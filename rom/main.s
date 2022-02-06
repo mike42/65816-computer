70ebd82604ab367e6937d0021e87fb603a12e2a0
Michael Billington <michael.billington@gmail.com>
2022-02-06 22:29:33 +1100

    latest firmware, saves and restores same task (I think)

--- rom/main.s
+++ rom/main.s
@@ -8,68 +8,141 @@ ACR = $c00b
 IFR = $c00b
 IER = $c00e
 
-.segment "CODE"
+.segment "BSS"
+next_task_sp: .res 2       ; Stack pointer of other task
+temp: .res 2
 
+.segment "CODE"
 reset:
-  ; switch to native mode
-  clc
+  clc                             ; switch to native mode
   xce
 
-  ; use 16-bit accumulator and index registers
-  .a16
+  ; Save context as if we are at task_2_main, so we can switch to it later.
+  .a16                            ; use 16-bit accumulator and index registers
   .i16
-  rep #%00110000                 ; set up stack
-  lda #$2000
+  rep #%0011000
+  lda #$3000                      ; set up stack, direct page at $3000
   tcs
+  tcd
+  ; emulate what is pushed to the stack before NMI is called: progam bank, program counter, processor status register
+  ; TODO suspect that there is a problem here..
+  phk                             ; program bank register, same as current code, will be 0 here.
+  .a8                             ; use 8-bit accumulator and index registers
+  .i8
+  sep #%0011000
+  lda #>task_2_main
+  pha
+  lda #<task_2_main
+  pha
+  ;  pea task_2_main                 ; 16 bit program counter for the start of this task
+  .a16                            ; use 16-bit accumulator and index registers
+  .i16
+  rep #%0011000
+  php                             ; processor status register
+  ; match what we push to the stack in the nmi routine
+  lda #0
+  tax
+  tay
+  pha                             ; Push A, X, Y
+  phx
+  phy
+  phb                             ; Push data bank, direct register
+  phd
+  tsc                             ; save stack pointer to next_task_sp
+  sta next_task_sp
 
-  ; use 8-bit accumulator and index registers
-  .a8
+  .a16                            ; use 16-bit accumulator and index registers
+  .i16
+  rep #%00110000
+  lda #$2000                      ; set up stack, direct page at $2000 for task_2_main
+  tcs
+  tcd
+
+  ; Set up the interrupt timer
+  .a8                             ; use 8-bit accumulator and index registers
   .i8
   sep #%00110000
-
-  ; set all VIA pins to output
-  lda #%11111111
+  lda #%11111111                  ; set all VIA pins to output
   sta DDRA
   sta DDRB
-
   ; set up timer 1
-  lda #%01000000        ; set ACR. first two bits = 01 is continuous interrupts for T1
+  lda #%01000000                 ; set ACR. first two bits = 01 is continuous interrupts for T1
   sta ACR
-  lda #%11000000        ; enable VIA interrupt for T1
+  lda #%11000000                ; enable VIA interrupt for T1
   sta IER
-
   ; set up a timer at ~65535 clock pulses.
-  lda #$ff              ; set T1 low-order counter
+  lda #$ff                      ; set T1 low-order counter
   sta T1C_L
-  lda #$ff              ; set T1 high-order counter
+  lda #$ff                      ; set T1 high-order counter
   sta T1C_H
 
-;  jmp task_1_main
-   jmp task_2_main
+  ; start running task 1
+  jmp task_1_main
+; jmp task_2_main
 
+; Task 1
 task_1_main:
+  .a8                             ; use 8-bit accumulator and index registers
+  .i8
+  sep #%00110000
 @repeat_1:
-  lda #%00000000
+  lda #%00000000                  ; alternate between two values
   sta PORTA
   lda #%00000011
   sta PORTA
-  jmp @repeat_1
+  jmp @repeat_1                   ; repeat forever
 
+; Task 2
 task_2_main:
-  lda #%01010101
+  .a8                             ; use 8-bit accumulator and index registers
+  .i8
+  sep #%00110000
+  lda #%01010101                  ; grab a start value
 @repeat_2:
-  ror
+  ror                             ; rotate right
   sta PORTB
-  jmp @repeat_2
+  jmp @repeat_2                   ; repeat forever
 
 nmi:
-  ; TODO is figuring out how to save the state completely here
+  .a16
+  .i16
+  rep #%00110000
+  ; Save task context to stack
+  pha       ; Push A, X, Y
+  phx
+  phy
+  phb       ; Push data bank, direct register
+  phd
+
+  ; Return to same task
+  tsc
+  sta next_task_sp
+  lda next_task_sp
+  tcs
+
+  ; swap stack pointer so that we will restore the other task
+;  tsc                     ; transfer current stack pointer to memory
+;  sta temp
+;  lda next_task_sp        ; load next stack pointer from memory
+;  tcs
+;  lda temp                ; previous task is up next
+;  sta next_task_sp
+
+  ; Clear interrut
   .a8
   .i8
   sep #%00110000  ; save X only (assumes it is 8 bits..)
-  phx
   ldx T1C_L       ; Clear the interrupt, side-effect of reading
+
+  .a16
+  .i16
+  rep #%00110000
+  ; Restore process context from stack, reverse order
+  pld       ; Pull direct register, data bank
+  plb
+  ply       ; Pull Y, X, A
   plx
+  pla
   rti
 
 irq:
