af9ce95f9afaf9495161af46404a61c943006507
Michael Billington <michael.billington@gmail.com>
2022-04-01 22:11:48 +1100

    update ROM to UART test (does not work on real hardware)

--- rom/main.s
+++ rom/main.s
@@ -1,129 +1,18 @@
-PORTB = $c000
-PORTA = $c001
-DDRB = $c002
-DDRA = $c003
-T1C_L = $c004
-T1C_H = $c005
-ACR = $c00b
-IFR = $c00b
-IER = $c00e
-
-.segment "BSS"
-next_task_sp: .res 2              ; Stack pointer of whichever task is not currently running
-temp: .res 2
+.import nmi, multitasking_test, uart_test
 
 .segment "CODE"
 reset:
   clc                             ; switch to native mode
   xce
-
-  ; Save context as if we are at task_2_main, so we can switch to it later.
+  ; Set up stack and run UART test first
   .a16                            ; use 16-bit accumulator and index registers
   .i16
   rep #%00110000
-  lda #$3000                      ; set up stack, direct page at $3000
+  lda #$0200                      ; set up stack
   tcs
-  tcd
-  ; emulate what is pushed to the stack before NMI is called: progam bank, program counter, processor status register
-  phk                             ; program bank register, same as current code, will be 0 here.
-  pea task_2_main                 ; 16 bit program counter for the start of this task
-  php                             ; processor status register
-  ; match what we push to the stack in the nmi routine
-  lda #0
-  tax
-  tay
-  pha                             ; Push A, X, Y
-  phx
-  phy
-  phb                             ; Push data bank, direct register
-  phd
-  tsc                             ; save stack pointer to next_task_sp
-  sta next_task_sp
-
-  lda #$2000                      ; set up stack, direct page at $2000 for task_2_main
-  tcs
-  tcd
-
-  ; Set up the interrupt timer
-  .a8                             ; use 8-bit accumulator and index registers
-  .i8
-  sep #%00110000
-  lda #%11111111                  ; set all VIA pins to output
-  sta DDRA
-  sta DDRB
-  ; set up timer 1
-  lda #%01000000                  ; set ACR. first two bits = 01 is continuous interrupts for T1
-  sta ACR
-  lda #%11000000                  ; enable VIA interrupt for T1
-  sta IER
-  ; set up a timer at ~65535 clock pulses.
-  lda #$ff                        ; set T1 low-order counter
-  sta T1C_L
-  lda #$ff                        ; set T1 high-order counter
-  sta T1C_H
-
-  ; start running task 1
-  jmp task_1_main
-
-; Task 1
-task_1_main:
-  .a8                             ; use 8-bit accumulator and index registers
-  .i8
-  sep #%00110000
-@repeat_1:
-  lda #%00000000                  ; alternate between two values
-  sta PORTA
-  lda #%00000011
-  sta PORTA
-  jmp @repeat_1                   ; repeat forever
-
-; Task 2
-task_2_main:
-  .a8                             ; use 8-bit accumulator and index registers
-  .i8
-  sep #%00110000
-  clc
-  lda #%01010101                  ; grab a start value
-@repeat_2:
-  ror                             ; rotate right
-  sta PORTB
-  jmp @repeat_2                   ; repeat forever
-
-nmi:
-  .a16
-  .i16
-  rep #%00110000
-  ; Save task context to stack
-  pha                             ; Push A, X, Y
-  phx
-  phy
-  phb                             ; Push data bank, direct register
-  phd
-
-  ; swap stack pointer so that we will restore the other task
-  tsc                             ; transfer current stack pointer to memory
-  sta temp
-  lda next_task_sp                ; load next stack pointer from memory
-  tcs
-  lda temp                        ; previous task is up next
-  sta next_task_sp
-
-  ; Clear interrut
-  .a8
-  .i8
-  sep #%00110000                  ; save X only (assumes it is 8 bits..)
-  ldx T1C_L                       ; Clear the interrupt, side-effect of reading
-
-  .a16
-  .i16
-  rep #%00110000
-  ; Restore process context from stack, reverse order
-  pld                             ; Pull direct register, data bank
-  plb
-  ply                             ; Pull Y, X, A
-  plx
-  pla
-  rti
+  jsr uart_test
+  ; Run multi-tasking test next.
+  jmp multitasking_test           ; Test multitasking afterward
 
 irq:
   rti
---
+++ rom/uart_test.s
@@ -0,0 +1,111 @@
+;
+; Test program for the SC16C752B dual UART from NXP
+;
+.export uart_test
+
+; Assuming /CSA is connected to IO1
+UART_BASE = $c200
+
+; UART registers
+; Not included: FIFO ready register, Xon1 Xon2 words.
+UART_RHR = UART_BASE        ; Receiver Holding Register (RHR) - R
+UART_THR = UART_BASE        ; Transmit Holding Register (THR) - W
+UART_IER = UART_BASE + 1    ; Interrupt Enable Register (IER) - R/W
+UART_IIR = UART_BASE + 2    ; Interrupt Identification Register (IIR) - R
+UART_FCR = UART_BASE + 2    ; FIFO Control Register (FCR) - W
+UART_LCR = UART_BASE + 3    ; Line Control Register (LCR) - R/W
+UART_MCR = UART_BASE + 4    ; Modem Control Register (MCR) - R/W
+UART_LSR = UART_BASE + 5    ; Line Status Register (LSR) - R
+UART_MSR = UART_BASE + 6    ; Modem Status Register (MSR) - R
+UART_SPR = UART_BASE + 7    ; Scratchpad Register (SPR) - R/W
+; Different meaning when LCR is logic 1
+UART_DLL = UART_BASE        ; Divisor latch LSB - R/W
+UART_DLM = UART_BASE + 1    ; Divisor latch MSB - R/W
+; Different meaning when LCR is %1011 1111 ($bh).
+UART_EFR = UART_BASE + 2    ; Enhanced Feature Register (EFR) - R/W
+UART_TCR = UART_BASE + 6    ; Transmission Control Register (TCR) - R/W
+UART_TLR = UART_BASE + 7    ; Trigger Level Register (TLR) - R/W
+
+.segment "CODE"
+uart_test:
+  .a8                       ; use 8-bit accumulator and index registers
+  .i8
+  sep #%00110000
+
+  lda #$80                  ; Enable divisor latches
+  sta UART_LCR
+  lda #1                    ; Set divisior to 1 - on a 1.8432 MHZ XTAL1, this gets 115200bps.
+  sta UART_DLL
+  lda #0
+  sta UART_DLM
+  lda #%00010111            ; Sets up 8-n-1
+  sta UART_LCR
+  lda #%00001111            ; Enable FIFO, set DMA mode 1
+  sta UART_FCR
+@again:
+  jsr test_print
+  jsr test_recv
+;  jsr test_ansi
+;  jmp @again
+  rts
+
+test_print:                 ; Send test string to UART
+  ldx #0
+@test_char:
+  lda test_string, X
+  beq @test_done
+  sta UART_THR
+  inx
+  jmp @test_char
+@test_done:
+  rts
+
+test_string: .asciiz "Hello, world!"
+;  .byte $1b
+;  .asciiz "[31;42mHello, world!"
+
+test_recv:                  ; Receive three characters and echo them back
+  jsr uart_recv_char
+  sta UART_THR
+  jsr uart_recv_char
+  sta UART_THR
+  jsr uart_recv_char
+  sta UART_THR
+  rts
+
+uart_recv_char:             ; Receive next char to A register
+  lda UART_LSR
+  and #%00000001
+  cmp #%00000001
+  bne uart_recv_char
+  lda UART_RHR
+  rts
+
+;test_ansi:
+;; Print ANSI code for clear
+;  ldx #0
+;@clear_char:
+;  lda ansi_clear_str, X
+;  beq @clear_done
+;  sta UART_THR
+;  inx
+;  jmp @clear_char
+;@clear_done:
+;; Print ANSI code for home
+;  ldx #0
+;@home_char:
+;  lda ansi_home_str, X
+;  beq @home_done
+;  sta UART_THR
+;  inx
+;  jmp @home_char
+;@home_done:
+;  rts
+;
+;ansi_clear_str:
+;  .byte $1b
+;  .asciiz "[J"
+;
+;ansi_home_str:
+;  .byte $1b
+;  .asciiz "[H"
