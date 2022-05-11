f92e43a5ded08877521c829283a2cbc1435afde2
Michael Billington <michael.billington@gmail.com>
2022-05-11 23:03:27 +1000

    add backspace, Ctrl+U to shell

--- software/kernel/shell.s
+++ software/kernel/shell.s
@@ -1,4 +1,4 @@
-.import uart_printz, uart_print_char, uart_recv_char
+.import uart_printz, uart_print_char, uart_recv_char, panic
 
 .export shell_main
 
@@ -29,57 +29,64 @@ shell_next_char:
     jsr uart_recv_char             ; wait for a character
     cmp #$0a                       ; return key pressed? - might need to be $0d
     beq @run_command               ; run the command
-    ldx shell_buffer_used          ; append any other key to buffer
-    cpx #64                        ; ignore char if buffer full
-    bcs shell_next_char
-    cmp #27                        ; Check for control characters 0-26, which are ^@, then ^A through ^Z
+    cmp #32                        ; Check for control characters 0-31, which are ^@, then ^A through ^Z, then ^[, ^\, ^], ^^, ^_
     bcc @ascii_control_code
-
+    cmp #$7f
+    beq @ascii_del
+    ldx shell_buffer_used          ; append any other key to buffer
+    cpx #64                        ; .. unless buffer full
+    bcs shell_next_char
     sta shell_buffer, X            ; ascii printable is assumed, save to buffer
     inx
     inx
     stx shell_buffer_used
     jsr uart_print_char
-
     jmp shell_next_char
 @run_command:
     jsr shell_newline
-    jsr shell_hello_main
-    jmp shell_next_command
-@ascii_control_code:                ; run a method from control code table
+    jmp shell_run_command
+@ascii_del:
+    lda #32
+@ascii_control_code:               ; run a method from control code table
     asl
     tax
     jsr (ascii_control_code_table, X)
     jmp shell_next_char
 
 ascii_control_code_table:
-ctrl_at: .word shell_shortcut_nop
-ctrl_a: .word shell_shortcut_nop    ; home
-ctrl_b: .word shell_shortcut_nop    ; end
-ctrl_c: .word shell_shortcut_nop    ; cancel command
-ctrl_d: .word shell_shortcut_exit
-ctrl_e: .word shell_shortcut_nop
-ctrl_f: .word shell_shortcut_nop    ; one character forward
-ctrl_g: .word shell_shortcut_nop    ; one character backwards
-ctrl_h: .word shell_shortcut_nop
-ctrl_i: .word shell_shortcut_nop
-ctrl_j: .word shell_shortcut_nop
-ctrl_k: .word shell_shortcut_nop
-ctrl_l: .word shell_shortcut_clear_screen
-ctrl_m: .word shell_shortcut_nop
-ctrl_n: .word shell_shortcut_nop
-ctrl_o: .word shell_shortcut_nop
-ctrl_p: .word shell_shortcut_nop
-ctrl_q: .word shell_shortcut_nop
-ctrl_r: .word shell_shortcut_nop
-ctrl_s: .word shell_shortcut_nop
-ctrl_t: .word shell_shortcut_nop
-ctrl_u: .word shell_shortcut_delete_to_line_start
-ctrl_v: .word shell_shortcut_nop
-ctrl_w: .word shell_shortcut_nop
-ctrl_x: .word shell_shortcut_nop
-ctrl_y: .word shell_shortcut_nop
-ctrl_z: .word shell_shortcut_nop
+.word shell_shortcut_nop           ; ^@
+.word shell_shortcut_nop           ; ^A home
+.word shell_shortcut_nop           ; ^B end
+.word shell_shortcut_nop           ; ^C cancel command
+.word shell_shortcut_exit          ; ^D
+.word shell_shortcut_nop           ; ^E
+.word shell_shortcut_nop           ; ^F one character forward
+.word shell_shortcut_nop           ; ^G one character backwards
+.word shell_shortcut_nop           ; ^H
+.word shell_shortcut_nop           ; ^I
+.word shell_shortcut_nop           ; ^J
+.word shell_shortcut_nop           ; ^K
+.word shell_shortcut_clear_screen  ; ^L
+.word shell_shortcut_nop           ; ^M
+.word shell_shortcut_nop           ; ^N
+.word shell_shortcut_nop           ; ^O
+.word shell_shortcut_nop           ; ^P
+.word shell_shortcut_nop           ; ^Q
+.word shell_shortcut_nop           ; ^R
+.word shell_shortcut_nop           ; ^S
+.word shell_shortcut_nop           ; ^T
+.word shell_shortcut_delete_to_line_start ; ^U
+.word shell_shortcut_nop           ; ^V
+.word shell_shortcut_nop           ; ^W
+.word shell_shortcut_nop           ; ^X
+.word shell_shortcut_nop           ; ^Y
+.word shell_shortcut_nop           ; ^Z
+.word shell_shortcut_esc           ; ^[
+.word shell_shortcut_nop           ; ^\
+.word shell_shortcut_nop           ; ^]
+.word shell_shortcut_nop           ; ^^
+.word shell_shortcut_nop           ; ^_
+.word shell_shortcut_backspace     ; ^?
 
 shell_shortcut_nop:
     rts
@@ -101,9 +108,40 @@ shell_shortcut_clear_screen:
     rts
 
 shell_shortcut_delete_to_line_start:
-    stz shell_buffer_used
-    jsr shell_reprint_line
+    lda #$08
+    ldx shell_buffer_used          ; skip if line empty (shell_buffer_used = 0)
+@backspace_next_char:
+    beq @backspace_done
+    dex
+    dex
+    jsr uart_print_char
+    cpx #0
+    jmp @backspace_next_char
+@backspace_done:
+    stz shell_buffer_used          ; all chars in buffer have now been backspaced
+    ldx #shell_clear_end_of_line   ; drop chars off end of line
+    jsr uart_printz
     rts
+
+shell_shortcut_esc:
+    ldx #shell_shortcut_esc_str
+    jsr uart_printz
+    rts
+shell_shortcut_esc_str: .asciiz "^["
+
+shell_shortcut_backspace:
+    ldx shell_buffer_used               ; skip if line empty (shell_buffer_used = 0)
+    beq @shell_shortcut_backspace_skip
+    dex
+    dex
+    stx shell_buffer_used
+    lda #$08                            ; backspace
+    jsr uart_print_char
+    ldx #shell_clear_end_of_line        ; drop chars off end of line
+    jsr uart_printz
+@shell_shortcut_backspace_skip:
+    rts
+shell_clear_end_of_line: .asciiz "\x1b[K"
 
 shell_newline:
     lda #$0a                       ; print a newline
@@ -115,19 +153,23 @@ shell_reprint_line:
     jsr uart_printz
     ldx #0
 @shell_reprint_line_next:
-
-    inx
-    inx
     cpx shell_buffer_used
-
-
-;
-;
-;    ldx #shell_buffer_used         ; throw a null at the end of the buffer
-;    stz shell_buffer, X
-;    ldx #shell_buffer
-
+    bcs @shell_reprint_line_done
+    lda shell_buffer, X
+    jsr uart_print_char
+    inx
+    inx
+    jmp @shell_reprint_line_next
+@shell_reprint_line_done:
     rts
+
+shell_run_command:
+    ; jsr shell_hello_main         ; TODO look up command in shell_builtin...
+    ldx #shell_not_found           ; pretend command is not found
+    jsr uart_printz
+    jsr shell_newline
+
+    jmp shell_next_command
 
 ; prompts and messages
 shell_not_found: .asciiz "Command not found"
@@ -140,6 +182,7 @@ shell_prompt: .asciiz "# "
 shell_builtin:
     .word shell_hello_bin, shell_hello_main
     .word shell_exit_bin, shell_exit_main
+    .word shell_clear_bin, shell_clear_main
 
 ;
 ; Built-in command: hello
