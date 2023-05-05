65270e43b1181da6978488d21bdae7c225463a97
Michael Billington <michael.billington@gmail.com>
2023-05-05 19:59:39 +1000

    switch test program to print "Hello, world!" instead of "z".

--- software/kernel_tmp/kernel_tmp.s
+++ software/kernel_tmp/kernel_tmp.s
@@ -1,11 +1,22 @@
-; kernel_tmp.s: A temporary placeholder kernel to test boot process.
+; kernel_tmp.s: A temporary placeholder program to test the boot process.
 
-ROM_PRINT_CHAR   := $00
+ROM_PRINT_STRING := $02
 
 .segment "CODE"
     .a16
     .i16
-demo_code:
-    lda #'z'
-    cop ROM_PRINT_CHAR
+    jmp __main
+
+__main:
+    ; set data bank register to equal program bank register
+    phk
+    plb
+
+    ; Print test string
+    ldx #(test_string & $ffff)
+    cop ROM_PRINT_STRING
+
+    ; Halt
     stp
+
+test_string: .asciiz "Hello, world!\r\n"
